990627bb3a6dac4c90b30b7d92de22a0300f2f7e
BOSS <bigboss1796@hotmail.com>
2023-12-21 13:18:33 +0700

    feat:React State ex1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState("Greeting Message");
+
+  const thaiGreeting = () => {
+    setCounter("สวัสดี!");
+  };
+
+  const englishGreeting = () => {
+    setCounter("Hi!");
+  };
+
+  const chineseGreeting = () => {
+    setCounter("你好!");
+  };
+
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{counter}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={thaiGreeting}>สวัสดี!</button>
+        <button onClick={englishGreeting}>Hi!</button>
+        <button onClick={chineseGreeting}>你好!</button>
       </div>
     </div>
   );
